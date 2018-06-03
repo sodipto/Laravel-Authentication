6ba6ccf07d8c62406ef9e9d7fe16fe2318db6e1a
Sodipto Saha <badhansaha444@gmail.com>
2018-06-03 23:46:49 +0600

    change App/config/auth.php for modification

--- config/auth.php
+++ config/auth.php
@@ -45,6 +45,11 @@ return [
             'driver' => 'token',
             'provider' => 'users',
         ],
+
+         'web' => [
+            'driver' => 'session',
+            'provider' => 'admin',
+        ],
     ],
 
     /*
@@ -74,6 +79,12 @@ return [
         //     'driver' => 'database',
         //     'table' => 'users',
         // ],
+
+        'admin' => [
+            'driver' => 'eloquent',
+            'model' => App\admin::class,
+        ],
+
     ],
 
     /*
@@ -94,6 +105,12 @@ return [
     'passwords' => [
         'users' => [
             'provider' => 'users',
+            'table' => 'password_resets',
+            'expire' => 60,
+        ],
+
+        'admin' => [
+            'provider' => 'admin',
             'table' => 'password_resets',
             'expire' => 60,
         ],
